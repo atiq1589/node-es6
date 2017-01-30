b63a45f9df21119a7a7ff43b3b5898ac94520ebe
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2017-01-30 16:46:53 +0600

    update index.js to log node mode

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
 const server = express();
+const SERVER_MODE = process.env.NODE_ENV;
+const PORT = process.env.PORT || 3000;
 
-
-server.listen(3000, ()=>{
-    console.log('server running on port 3000');
+server.listen(PORT, ()=>{
+    console.log(`server running on port ${PORT}`);
+    console.log(`server running on ${SERVER_MODE}`);
 });
 
 server.get('/', (req, res) => {
