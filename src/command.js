8efe1e0ca7475421a4e7c8bd38866184dd04d453
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2017-01-31 23:00:39 +0600

    add command.js to create new module

---
+++ src/command.js
@@ -0,0 +1,29 @@
+
+let fs = require('fs');
+var exec = require('child_process').exec;
+
+class Command {
+    constructor() {
+        this.rootDir = __dirname + '/app/';
+        this.args = process.argv;
+        this.args.splice(0, 1);
+        this.args.splice(0, 1);
+    }
+}
+
+(function () {
+    let cmd = new Command();
+    switch (cmd.args[0]) {
+        case 'newmodule':
+            let dir = cmd.rootDir + cmd.args[1];
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir);
+                fs.closeSync(fs.openSync(dir + '/urls.js', 'w'));
+                fs.closeSync(fs.openSync(dir + '/view.js', 'w'));
+            } else {
+                console.error("Module already exists");
+            }
+            break;
+    }
+
+})();
