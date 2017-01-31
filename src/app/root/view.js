167c20f8c235aa4fe46f9bd6970c99ea8b522547
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2017-01-31 15:59:17 +0600

    add root module to send index.html

---
+++ src/app/root/view.js
@@ -0,0 +1,14 @@
+import {render_file} from '../../render';
+
+export class IndexView {
+    constructor() {
+        this.templateUrl = '/../public/index.html';
+    }
+    RenderTemplate() {
+        let data = {
+            "name": "Alan", "hometown": "Somewhere, TX",
+            "kids": [{ "name": "Jimmy", "age": "12" }, { "name": "Sally", "age": "4" }]
+        };
+        return render_file(this.templateUrl, data);
+    }
+}
