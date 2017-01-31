ef951ff311d83024a8cd1f1f7354bf483ba17367
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2017-01-31 15:35:57 +0600

    add file renderer

---
+++ src/render.js
@@ -0,0 +1,31 @@
+import handlebars from 'handlebars';
+import fs from 'fs';
+
+export class Renderer {
+    /**
+     * This method will render a file 
+     * It will currently use handlebars
+     */
+    constructor(filePath, data) {
+        this.filePath = __dirname + filePath;
+        this.data = data;
+        this.renderedFile = "";
+        this._RenderFile();
+    }
+    _RenderFile() {
+        try {
+            let file = fs.readFileSync(this.filePath, 'utf-8');
+            let template = handlebars.compile(file);
+            this.renderedFile = template(this.data);
+            return this.renderedFile;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    }
+}
+
+export function render_file(filePath, data) {
+    let r = new Renderer(filePath, data);
+    return r.renderedFile;
+}
